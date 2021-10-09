de6bda5b845c488fe71cb38ffbfef6dc5d5774da
YoojinMa <geronimo9595@naver.com>
2021-10-09 23:56:05 +0900

    [Add Study] print(a ^ b)

--- PYTHON Algorithm.py
+++ PYTHON Algorithm.py
@@ -38,6 +38,7 @@ print(format(a, ".2f")) : 실수 a를 소수점 두 번째 자리(".2f")까지 �
 print(bool(a)) : a가 0이면 False, 0이 아니면 True 출력
 print(not a) : a가 False이면 반대인 True 출력, True면 False 출력
 print(~ a) : a를 비트단위로 참/거짓을 바꾼 후 정수로 출력
-print(a & b) : a와 b를 비트단위로 and 연산한 후 그 결과를 정수로 출력
+print(a & b) : a와 b를 비트단위로 and 연산(둘 다 1인 부분의 자리만 1로 변경)한 후 그 결과를 정수로 출력
 print(a | b) : a와 b를 비트단위로 or 연산(둘 중 하나라도 1인 자리를 1로 변경)한 후 그 결과를 정수로 출력
+print(a ^ b) : a와 b를 비트단위로 xor 연산(서로 다를 때 1)한 후 그 결과를 정수로 출력
 '''
